de6373e3d868cd7ece939c892dea883b626039bc
zhaoshanshan <zhaoshanshan.lucky@bytedance.com>
2020-12-13 15:55:01 +0800

    feat: 继续爬

--- src/crawler/esteelauder/esteelauder.py
+++ src/crawler/esteelauder/esteelauder.py
@@ -7,10 +7,9 @@ from bs4 import BeautifulSoup
 # BeautifulSoup用于解析HTML响应
 
 import json
-
-url = "https://www.esteelauder.com.cn/product/649/29657/product-catalog/envy-rouge"
 json_filename = './esteelauder.json'
 
+url = 'https://www.esteelauder.com.cn/products/649/product-catalog'
 # 请求官网的URL，获取其页面内容
 wbdata = requests.get(url).text
 # 对获取到的文本进行解析
@@ -19,24 +18,39 @@ soup = BeautifulSoup(wbdata,'html.parser')
 # 从解析文件中通过select选择器定位指定的元素，返回一个列表
 # select 的功能跟find和find_all 一样用来选取特定的标签，它的选取规则依赖于css
 # 支持通过标签名/类名/id/属性等查找
-lips_titles = soup.select("a.swatch")
-lips_color = soup.select(".swatch__container .swatch--1 ")
+type_list = soup.select(".mpp__product-grid a.product_brief__image-container")
 
-# 对返回的列表进行遍历
-result = []
-for index, val in enumerate(lips_titles):
-    # 提取内容和颜色信息
-    # title = val.get_text()
-    name = val.get("name")
-    color = lips_color[index].get('style').split(':')[1].split(';')[0]
+print('shanshan href', type_list)
+
+result = {}
+
+for index, val in enumerate(type_list):
+    href = val.get("href")
+    url = "https://www.esteelauder.com.cn"+ href
+    wbdata = requests.get(url).text
+    soup = BeautifulSoup(wbdata,'html.parser')
+    lips_type = soup.select("div.product-full__description .product-full__subtitle")[0].get_text()
+    lips_titles = soup.select("ul.js-shade-picker a.swatch")
+    lips_color = soup.select("ul.js-shade-picker .swatch__container .swatch--1 ")
+
+    result[lips_type] = []
+
+    for index, val in enumerate(lips_titles):
+        # 提取内容和颜色信息
+        # title = val.get_text()
+        name = val.get("name")
+        color = lips_color[index].get('style').split(':')[1].split(';')[0]
+        
+        data = {
+            # '标题':title,
+            u'info': name,
+            u'color': color,
+        }
+        result[lips_type].append(data)
+        print('data', data)
+
     
-    data = {
-        # '标题':title,
-        u'info': name,
-        u'color': color,
-    }
-    result.append(data)
-    print('data', data)
+print('shanshan result', result)
 
 with open(json_filename, 'w') as f:
     json.dump({'esteelauder': result}, f)
